d900e229f20a768d7a9892b745c29d3d27299763
krisvanneste <kris.vanneste@oma.be>
2018-06-05 09:57:53 +0000

    Added macroseismic.py containing color palettes and norms for macroseismic intensity scales.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/mapping/layeredbasemap/trunk@4641 40b490c5-b4d9-47cb-8714-9bef99b524d5

---
+++ cm/macroseismic.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib
+
+from norm import PiecewiseLinearNorm
+
+
+usgs_rgb = np.array([(255, 255, 255),
+			(204, 212, 251),
+			(151, 239, 253),
+			(125, 253, 202),
+			(180, 253, 88),
+			(252, 234, 2),
+			(255, 180, 3),
+			(255, 77, 0),
+			(234, 0, 0)], dtype='f')
+usgs_rgb /= 255.
+
+usgs_cmap = matplotlib.colors.LinearSegmentedColormap.from_list("USGS_macroseismic",
+															usgs_rgb, N=len(usgs_rgb))
+usgs_cmap.set_under('w')
+usgs_cmap.set_bad('w')
+usgs_norm = PiecewiseLinearNorm(range(1, 10))
+
+
+rob_rgb = np.array([(194, 202, 204),
+			(40, 247, 255),
+			(6, 251, 23),
+			(248, 254, 24),
+			(255, 128, 0),
+			(249, 3, 16),
+			(128, 0, 128)], dtype='f')
+rob_rgb /= 255.
+
+rob_cmap = matplotlib.colors.LinearSegmentedColormap.from_list("ROB_macroseismic",
+															rob_rgb, N=len(rob_rgb))
+rob_cmap.set_under('w')
+rob_cmap.set_bad('w')
+rob_norm = PiecewiseLinearNorm(range(1, 8))
