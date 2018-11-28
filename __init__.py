22d23deecb94ff9883d6989a10b5a91a0c635789
krisvanneste <kris.vanneste@oma.be>
2018-11-28 12:36:00 +0000

    Made import mechanism python3-compatible.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/mapping/layeredbasemap/trunk@4810 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- __init__.py
+++ __init__.py
@@ -1,6 +1,34 @@
-#
-# Empty file necessary for python to recognise directory as package
-#
+"""
+layeredbasemap
+
+Module to create maps with Basemap using the GIS layer philosophy,
+where each layer is defined by a dataset and style.
+
+Author: Kris Vanneste, Royal Observatory of Belgium
+"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+
+## Make relative imports work in Python 3
+import importlib
+
+
+## Reloading mechanism
+try:
+	reloading
+except NameError:
+	## Module is imported for the first time
+	reloading = False
+else:
+	## Module is reloaded
+	reloading = True
+	try:
+		## Python 3
+		from importlib import reload
+	except ImportError:
+		## Python 2
+		pass
 
 
 ## Test GDAL environment
@@ -14,33 +42,31 @@ for key in gdal_keys:
 		print("Warning: %s points to non-existing directory %s" % (key, os.environ[key]))
 
 
-import styles
-reload(styles)
+## Import submodules
 
-from styles import (FontStyle, TextStyle, DefaultTitleTextStyle, PointStyle,
-					LineStyle, PolygonStyle, FocmecStyle, CompositeStyle,
-					ThematicStyleIndividual, ThematicStyleRanges, ThematicStyleGradient,
-					ThematicStyleColormap, ColorbarStyle, GridStyle, LegendStyle,
-					ScalebarStyle, MapBorderStyle, GraticuleStyle, GridImageStyle,
-					ImageStyle, HillshadeStyle, WMSStyle, FrontStyle, VectorStyle,
-					PiechartStyle)
+## styles
+if not reloading:
+	styles = importlib.import_module('.styles', package=__name__)
+else:
+	reload(styles)
+from .styles import *
 
+## data_types
+if not reloading:
+	data_types = importlib.import_module('.data_types', package=__name__)
+else:
+	reload(data_types)
+from .data_types import *
 
-import data_types
-reload(data_types)
+## cm
+if not reloading:
+	cm = importlib.import_module('.cm', package=__name__)
+else:
+	reload(cm)
 
-from data_types import (BuiltinData, PointData, MultiPointData, LineData, MultiLineData,
-						PolygonData, MultiPolygonData, FocmecData, CircleData, MaskData,
-						CompositeData, GridData, MeshGridData, UnstructuredGridData,
-						GdalRasterData, GisData, GreatCircleData, ImageData, WMSData,
-						WCSData, MeshGridVectorData, TextData, MultiTextData,
-						PiechartData)
-
-import cm
-reload(cm)
-
-
-import layered_basemap
-reload(layered_basemap)
-
-from layered_basemap import (MapLayer, LayeredBasemap)
+## layered_basemap
+if not reloading:
+	layered_basemap = importlib.import_module('.layered_basemap', package=__name__)
+else:
+	reload(layered_basemap)
+from .layered_basemap import *
