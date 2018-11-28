5253f04f93e6647ed37674f858c6caf712477a17
krisvanneste <kris.vanneste@oma.be>
2018-11-28 12:54:25 +0000

    Minor changes to make code python3-compatible.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/mapping/layeredbasemap/trunk@4818 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- cm/macroseismic.py
+++ cm/macroseismic.py
@@ -1,7 +1,10 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+
 import numpy as np
 import matplotlib
 
-from norm import PiecewiseLinearNorm
+from .norm import PiecewiseLinearNorm
 
 
 usgs_rgb = np.array([(255, 255, 255),
