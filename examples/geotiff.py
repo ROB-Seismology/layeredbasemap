d7d14477867779f2c6e05eb7ea56eff9f649c234
krisvanneste <kris.vanneste@oma.be>
2019-03-14 22:29:44 +0000

    Made separate layers for GeoTIFF image and GeoTIFF raster.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/mapping/layeredbasemap/trunk@5202 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- examples/geotiff.py
+++ examples/geotiff.py
@@ -20,15 +20,27 @@ title = "GeoTIFF example"
 
 layers = []
 
-## GeoTiff / WCS
-geotiff_filespec = "D:\\seismo-gis\\collections\\ASTER_GDEM\\GEOTIFF\\ASTGTM_N50E006.tif"
-#geotiff_filespec = "C:\\Temp\\ASTGTM_N50E006.tif"
+## GeoTiff image
 #geotiff_filespec = "C:\\Temp\\matplotlib.tif"
+#geotiff_filespec = "E:\\Public\\Data RegWallonne\\Hazard Maps\\rock\\Tr=02500yr\\HazardMap_T=0.0s_Tr=02500yr.TIF"
+#geotiff_filespec = "D:\\GIS-data\\Belgium\\DEM\\ngi_dem1_Lambert.TIF"
+geotiff_filespec = "C:\\Temp\\macromap.TIF"
+gdal_data = lbm.GdalRasterData(geotiff_filespec, band_nr=0, down_sampling=1)
+style = lbm.GridImageStyle()
+layer = lbm.MapLayer(gdal_data, style)
+#layers.append(layer)
+
+
+## WCS / GeoTiff raster
+#geotiff_filespec = "E:\\Home\\_kris\\Projects\\2015 - RegionWallonne\\Figures\\Hazard maps\\rock\\HazardMap_T=0.000s_2.5E+03yr.TIF"
+geotiff_filespec = "D:\\seismo-gis\\collections\\ASTER_GDEM\\GEOTIFF\\ASTGTM_N50E006.tif"
 gdal_data = lbm.GdalRasterData(geotiff_filespec, band_nr=1, down_sampling=3)
+"""
 url = 'http://seishaz.oma.be:8080/geoserver/wcs'
 layer_name, resolution, bbox = 'ngi:DTM10k', 1000, []
 #layer_name, resolution, bbox = 'nasa:ASTER_GDEM_V2', 0.02, region[::2]+region[1::2]
 wcs_data = lbm.WCSData(url, layer_name, resolution=resolution, bbox=bbox)
+"""
 colorbar_style = lbm.ColorbarStyle("Elevation (m)")
 #colorbar_style = None
 #hillshade_style = lbm.HillshadeStyle(azimuth=45,elevation_angle=30, scale=0.1, color_map="copper")
@@ -38,9 +50,10 @@ cmap, norm = lbm.cm.from_cpt_city(r"td/DEM_poster")
 tsc = lbm.ThematicStyleColormap(color_map=cmap, vmin=0, vmax=700)
 #tsc.color_map = None
 style = lbm.GridStyle(color_map_theme=tsc, colorbar_style=colorbar_style, line_style=None, pixelated=True, hillshade_style=hillshade_style)
-#style = lbm.GridImageStyle()
-layer = lbm.MapLayer(wcs_data, style)
+#layer = lbm.MapLayer(wcs_data, style)
+layer = lbm.MapLayer(gdal_data, style)
 layers.append(layer)
+
 
 ## Country borders
 data = lbm.BuiltinData("countries")
